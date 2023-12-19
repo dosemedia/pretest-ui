f4866a59580e6f29978c2fd02eb566043b7f5e81
bradyAdger12 <brady@traildock.com>
2023-12-19 11:33:56 -0700

    Change default hasura base url

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,7 +1,7 @@
 import { Client, fetchExchange } from 'urql'
 import { authStore } from './stores/stores'
 export const client = new Client({
-  url: (import.meta.env.VITE_HASURA_BASE_URL || 'http://localhost:8080') + '/v1/graphql',
+  url: (import.meta.env.VITE_HASURA_BASE_URL || 'http://localhost:8081') + '/v1/graphql',
   fetchOptions: () => {
     // const token = localStorage.getItem('auth.token')
     // return token ? { headers: { Authorization: `Bearer ${token}` } } : {}
