c5ed65289b958ca582e267af665f2c70d2808f63
bradyAdger12 <brady@traildock.com>
2024-01-04 14:18:54 -0700

    Write login tests

--- src/__tests__/Auth.test.tsx
+++ src/__tests__/Auth.test.tsx
@@ -6,14 +6,15 @@ import RegisterForm from '../components/auth/RegisterForm';
 import { DateTime } from 'luxon';
 import * as router from 'react-router'
 import { authStore } from '../stores/stores';
+import LoginForm from '../components/auth/LoginForm';
 
 const queryClient = new QueryClient()
 const userEmail = `test_user_${DateTime.now().toMillis()}@user.com`
 const navigate = vi.fn()
+beforeEach(() => {
+  vi.spyOn(router, 'useNavigate').mockImplementation(() => navigate)
+})
 describe("User register authentication", () => {
-  beforeEach(() => {
-    vi.spyOn(router, 'useNavigate').mockImplementation(() => navigate)
-  })
   it("User cannot register without email present", async () => {
     const result = render(<QueryClientProvider client={queryClient}><Router><RegisterForm /></Router></QueryClientProvider>)
     const emailInput = result.container.querySelector('#email')
@@ -55,17 +56,14 @@ describe("User register authentication", () => {
     fireEvent.click(loginButton)
     await waitFor(async () => {
       expect(navigate).toHaveBeenCalledWith('/');
-      const errorMessage = result.container.querySelector('#error_message')
-      expect(errorMessage).not.toBeInTheDocument()
     })
   })
   it("User cannot use taken email", async () => {
     vi.spyOn(authStore, 'register').mockImplementation(async () => {
-      console.log('test')
       throw new Error('account already exists')
     });
     const result = render(<QueryClientProvider client={queryClient}><Router><RegisterForm /></Router></QueryClientProvider>)
-    const passwordInput = result.container.querySelector('#passwordd')!
+    const passwordInput = result.container.querySelector('#password')!
     const emailInput = result.container.querySelector('#email')!
     const legalCheckboxInput = result.container.querySelector('#legal_checkbox')!
     fireEvent.change(passwordInput, { target: { value: 'foobar' } })
@@ -81,55 +79,64 @@ describe("User register authentication", () => {
 })
 
 
-// describe("User login authentication", async () => {
-//   it("User cannot login with a password present", async () => {
-//     const result = render(<QueryClientProvider client={queryClient}><Router><LoginForm /></Router></QueryClientProvider>)
-//     const emailInput = result.container.querySelector('#email')
-//     fireEvent.change(emailInput!, { target: { value: userEmail } })
-//     const loginButton = result.container.querySelector('#login_button')
-//     fireEvent.click(loginButton!)
-//     await waitFor(() => {
-//       const errorMessage = screen.getByText(/did not match/)
-//       expect(errorMessage).toBeVisible()
-//     })
-//   })
-//   it("User cannot login with an email present", async () => {
-//     const result = render(<QueryClientProvider client={queryClient}><Router><LoginForm /></Router></QueryClientProvider>)
-//     const passwordInput = result.container.querySelector('#password')
-//     fireEvent.change(passwordInput!, { target: { value: 'foobar' } })
-//     const loginButton = result.container.querySelector('#login_button')
-//     fireEvent.click(loginButton!)
-//     await waitFor(() => {
-//       const errorMessage = screen.getByText(/Email is required/)
-//       expect(errorMessage).toBeVisible()
-//     })
-//   })
-//   it("User cannot login without correct email and password", async () => {
-//     const result = render(<QueryClientProvider client={queryClient}><Router><LoginForm /></Router></QueryClientProvider>)
-//     const passwordInput = result.container.querySelector('#password')
-//     const emailInput = result.container.querySelector('#email')
-//     fireEvent.change(passwordInput!, { target: { value: 'foobar' } })
-//     fireEvent.change(emailInput!, { target: { value: DateTime.now().toString() + '@user.com' } })
-//     const loginButton = result.container.querySelector('#login_button')
-//     fireEvent.click(loginButton!)
-//     await waitFor(() => {
-//       const errorMessage = screen.getByText(/User not found/)
-//       expect(errorMessage).toBeVisible()
-//     })
-//   })
-//   it("User can login with correct email and password", async () => {
-//     const result = render(<QueryClientProvider client={queryClient}><Router><LoginForm /></Router></QueryClientProvider>)
-//     const passwordInput = result.container.querySelector('#password')
-//     const emailInput = result.container.querySelector('#email')
-//     fireEvent.change(passwordInput!, { target: { value: 'foobar' } })
-//     fireEvent.change(emailInput!, { target: { value: userEmail } })
-//     const loginButton = result.container.querySelector('#login_button')
-//     fireEvent.click(loginButton!)
-//     await new Promise(resolve => setTimeout(resolve, 2000));
-//     await waitFor(() => {
-//       const errorMessage = result.container.querySelector('#error_message')
-//       expect(errorMessage).not.toBeInTheDocument()
-//       authStore.token = ''
-//     })
-//   })
-// })
+describe("User login authentication", async () => {
+  it("User cannot login without a password present", async () => {
+    vi.spyOn(authStore, 'login').mockImplementation(async () => {
+      throw new Error('Email or password did not match')
+    });
+    const result = render(<QueryClientProvider client={queryClient}><Router><LoginForm /></Router></QueryClientProvider>)
+    const emailInput = result.container.querySelector('#email')
+    fireEvent.change(emailInput!, { target: { value: userEmail } })
+    const loginButton = result.container.querySelector('#login_button')
+    fireEvent.click(loginButton!)
+    await waitFor(() => {
+      const errorMessage = screen.getByText(/did not match/)
+      expect(errorMessage).toBeVisible()
+    })
+  })
+  it("User cannot login without an email present", async () => {
+    vi.spyOn(authStore, 'login').mockImplementation(async () => {
+      throw new Error('Email is required')
+    });
+    const result = render(<QueryClientProvider client={queryClient}><Router><LoginForm /></Router></QueryClientProvider>)
+    const passwordInput = result.container.querySelector('#password')
+    fireEvent.change(passwordInput!, { target: { value: 'foobar' } })
+    const loginButton = result.container.querySelector('#login_button')
+    fireEvent.click(loginButton!)
+    await waitFor(() => {
+      const errorMessage = screen.getByText(/Email is required/)
+      expect(errorMessage).toBeVisible()
+    })
+  })
+  it("User cannot login without correct email and password", async () => {
+    vi.spyOn(authStore, 'login').mockImplementation(async () => {
+      throw new Error('User not found')
+    });
+    const result = render(<QueryClientProvider client={queryClient}><Router><LoginForm /></Router></QueryClientProvider>)
+    const passwordInput = result.container.querySelector('#password')
+    const emailInput = result.container.querySelector('#email')
+    fireEvent.change(passwordInput!, { target: { value: 'foobar' } })
+    fireEvent.change(emailInput!, { target: { value: DateTime.now().toString() + '@user.com' } })
+    const loginButton = result.container.querySelector('#login_button')
+    fireEvent.click(loginButton!)
+    await waitFor(() => {
+      const errorMessage = screen.getByText(/User not found/)
+      expect(errorMessage).toBeVisible()
+    })
+  })
+  it("User can login with correct email and password", async () => {
+    vi.spyOn(authStore, 'login').mockImplementation(async () => {
+      await Promise.resolve()
+    });
+    const result = render(<QueryClientProvider client={queryClient}><Router><LoginForm /></Router></QueryClientProvider>)
+    const passwordInput = result.container.querySelector('#password')
+    const emailInput = result.container.querySelector('#email')
+    fireEvent.change(passwordInput!, { target: { value: 'foobar' } })
+    fireEvent.change(emailInput!, { target: { value: userEmail } })
+    const loginButton = result.container.querySelector('#login_button')
+    fireEvent.click(loginButton!)
+    await waitFor(() => {
+      expect(navigate).toHaveBeenCalledWith('/');
+    })
+  })
+})
